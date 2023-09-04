5a3e0c19191b8eb74ef8a4017395d426342d53f9
kaanakdogan <kaan.akdogann@gmail.com>
2023-09-04 23:08:19 +0300

    lsp fix

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,7 +1,7 @@
-local status, lsp = pcall(require, 'lsp-zero')
-if (not status) then return end
-
+local lsp = require('lsp-zero')
 lsp.preset('recommended')
+
+vim.lsp.set_log_level("trace")
 
 lsp.ensure_installed({
     'tsserver',
@@ -14,62 +14,9 @@ lsp.ensure_installed({
     'tailwindcss',
 })
 
+-- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
-local lspconfig = require("lspconfig")
-
-lspconfig.eslint.setup({
-    on_attach = function(client, bufnr)
-        client.server_capabilities.document_formatting = true
-        client.server_capabilities.goto_definition = true
-        client.server_capabilities.documentFormattingProvidier = true
-        client.server_capabilities.documentRangeFormattingProvider = true
-
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-        })
-    end,
-
-    settings = {
-        ['codeActionOnSave'] = {
-            enable = true,
-            mode = "all"
-        },
-        ['workingDirectory'] = {
-            mode = "auto"
-        }
-    }
-})
-
-lspconfig.tsserver.setup({})
-lspconfig.html.setup({})
-lspconfig.cssls.setup({})
-lspconfig.tailwindcss.setup({})
-lspconfig.cssmodules_ls.setup({})
-
-lspconfig.lua_ls.setup {
-    on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT'
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                    library = { vim.env.VIMRUNTIME }
-                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                    -- library = vim.api.nvim_get_runtime_file("", true)
-                }
-            })
-
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-        end
-        return true
-    end
-}
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -77,9 +24,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
-
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
@@ -111,9 +57,13 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "<leader>gr", require('telescope.builtin').lsp_references, {})
 end)
 
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
 lsp.format_on_save({
     format_opts = {
         async = false,
@@ -127,10 +77,25 @@ lsp.format_on_save({
     }
 })
 
-lsp.setup()
+local lspconfig = require("lspconfig")
 
-vim.diagnostic.config({
-    virtual_text = true
+lspconfig.eslint.setup({
+    on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = true
+        client.server_capabilities.documentRangeFormattingProvider = true
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            command = "EslintFixAll",
+        })
+    end,
 })
 
+lspconfig.tsserver.setup({})
+lspconfig.html.setup({})
+lspconfig.cssls.setup({})
+lspconfig.tailwindcss.setup({})
+lspconfig.cssmodules_ls.setup({})
+
+lsp.setup()
 require("luasnip.loaders.from_vscode").lazy_load()
